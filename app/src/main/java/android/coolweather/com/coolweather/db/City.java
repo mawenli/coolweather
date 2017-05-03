a3da663d2887d2221a46b896e1633dbd9b2f9a3e
Tokay_ <1151896934@qq.com>
2017-05-03 20:00:46 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/City.java
@@ -0,0 +1,50 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Mr.ye on 2017/5/3.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;//记录市的名字
+
+    private int cityCode;//记录市的代号
+
+    private  String provinceId;//记录当前市所属县的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(String provinceId) {
+        this.provinceId = provinceId;
+    }
+}
